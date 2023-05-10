dd1822bfb6402f49ddf174b50a6e82aa5b885183
gilyeon00 <12venus08@naver.com>
2023-05-10 19:36:48 +0900

    [#1]Feat: Create AutoDebit Entity

---
+++ src/main/java/com/kb/jjan/domain/bank/autoDebit/AutoDebit.java
@@ -0,0 +1,39 @@
+package com.kb.jjan.domain.bank.autoDebit;
+
+
+import com.kb.jjan.domain.user.User;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@ToString(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "auto_debits")
+public class AutoDebit {
+    @Id
+    @GeneratedValue(
+            strategy= GenerationType.SEQUENCE,
+            generator="DEBIT_ID"
+    )
+    @Column(name = "debit_id")
+    private Long debitId;
+
+    @ManyToOne
+    @JoinColumn(name = "auto_send_user", nullable = false)
+    private User autoSendUser;
+
+    @ManyToOne
+    @JoinColumn(name = "auto_received_user", nullable = false)
+    private User autoReceivedUser;
+
+    @Column(length = 7, columnDefinition = "NUMBER(7)", nullable = false)
+    private int price;
+
+    @Column(name = "debit_msg", length = 20)
+    private int debitMsg;
+}
+
