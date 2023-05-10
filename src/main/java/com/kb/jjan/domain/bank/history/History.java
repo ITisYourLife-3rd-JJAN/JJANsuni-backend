a76d290d57515f5fa08e7e7a7ee5149dcc718c3f
gilyeon00 <12venus08@naver.com>
2023-05-10 19:23:04 +0900

    [#1]Feat: Create History Entity

---
+++ src/main/java/com/kb/jjan/domain/bank/history/History.java
@@ -0,0 +1,34 @@
+package com.kb.jjan.domain.bank.history;
+
+import com.kb.jjan.domain.user.User;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@ToString(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "histories")
+public class History {
+    @Id
+    @GeneratedValue(
+            strategy= GenerationType.SEQUENCE,
+            generator="HISTORY_ID"
+    )
+    @Column(name = "history_id")
+    private Long historyId;
+
+    @ManyToOne
+    @JoinColumn(name = "send_user", nullable = false)
+    private User sendUser;
+
+    @ManyToOne
+    @JoinColumn(name = "received_user", nullable = false)
+    private User receivedUser;
+
+    @Column(length = 7, columnDefinition = "NUMBER(7)")
+    private Long price;
+}
