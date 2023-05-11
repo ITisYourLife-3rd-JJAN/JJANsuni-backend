258eb1ab08263001f01b5d7cdf0248245ed7e086
gilyeon00 <12venus08@naver.com>
2023-05-11 17:59:42 +0900

    [#1]Feat: Create ResultResponse Format

---
+++ src/main/java/com/kb/jjan/global/result/ResultCode.java
@@ -0,0 +1,17 @@
+package com.kb.jjan.global.result;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ResultCode {
+
+    // 도메인 별로 나눠서 관리(ex: User 도메인, UserMission 도메인 ..)
+    // user
+    USER_REGISTRATION_SUCCESS("U001", "200", "사용자가 정상적으로 등록되었습니다.");
+
+    private final String code;
+    private final String status;
+    private final String message;
+}
---
+++ src/main/java/com/kb/jjan/global/result/ResultResponse.java
@@ -0,0 +1,27 @@
+package com.kb.jjan.global.result;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Getter;
+
+@Getter
+public class ResultResponse<T> {
+
+    private final String status;
+    private final String message;
+    private Object data;
+
+
+    @JsonCreator
+    public ResultResponse(ResultCode resultCode) {
+        this.status = resultCode.getStatus();
+        this.message = resultCode.getMessage();
+    }
+
+    @JsonCreator
+    public ResultResponse(String status, ResultCode resultCode, Object data) {
+        this.status = status;
+        this.message = resultCode.getMessage();
+        this.data = data;
+    }
+
+}
