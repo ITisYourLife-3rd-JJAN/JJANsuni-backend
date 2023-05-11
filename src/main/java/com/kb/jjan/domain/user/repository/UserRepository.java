f3a9a83590ada64d5614edacea8ec4e9fc7dce93
gilyeon00 <12venus08@naver.com>
2023-05-11 17:59:56 +0900

    [#1]Feat: Create UserRepository

---
+++ src/main/java/com/kb/jjan/domain/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.kb.jjan.domain.user.repository;
+
+import com.kb.jjan.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
