d0291866d07338e940374e1a8ae452a11ce9da63
gilyeon00 <12venus08@naver.com>
2023-05-12 15:14:28 +0900

    [#1]Feat: Create UserMission sample create-logic

--- src/main/java/com/kb/jjan/domain/mission/userMission/service/UserMissionService.java
+++ src/main/java/com/kb/jjan/domain/mission/userMission/service/UserMissionService.java
@@ -1,13 +1,33 @@
 package com.kb.jjan.domain.mission.userMission.service;
 
+import com.kb.jjan.domain.mission.mission.Mission;
+import com.kb.jjan.domain.mission.mission.repository.MissionRepository;
+import com.kb.jjan.domain.mission.userMission.UserMission;
+import com.kb.jjan.domain.mission.userMission.dto.UserMissionRequest;
 import com.kb.jjan.domain.mission.userMission.repository.UserMissionRepository;
+import com.kb.jjan.domain.user.User;
+import com.kb.jjan.domain.user.dto.UserRequest;
+import com.kb.jjan.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 @RequiredArgsConstructor
 public class UserMissionService {
 
     private final UserMissionRepository userMissionRepository;
+    private final MissionRepository missionRepository;
+    private final UserRepository userRepository;
+
+
+    public void registerUserMissionHistory(@RequestBody UserMissionRequest userMissionRequest) throws Exception {
+        Mission solvedMission = missionRepository.getReferenceById(userMissionRequest.getSolvedMissionId());
+        User solvedUser = userRepository.getReferenceById(userMissionRequest.getSolvedUserId());
+
+        UserMission userMission = userMissionRequest.toEntity(solvedMission, solvedUser );
+        userMissionRepository.save(userMission);
+    }
+
 
 }
