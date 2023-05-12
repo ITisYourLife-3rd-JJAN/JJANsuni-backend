9bd8f6a3c5910a77ef2eb52a24dd9b06e4f34080
gilyeon00 <12venus08@naver.com>
2023-05-12 10:45:26 +0900

    [#1]Feat: Create createQuiz logic in MissionService

---
+++ src/main/java/com/kb/jjan/domain/mission/mission/service/MissionService.java
@@ -0,0 +1,20 @@
+package com.kb.jjan.domain.mission.mission.service;
+
+
+import com.kb.jjan.domain.mission.mission.Mission;
+import com.kb.jjan.domain.mission.mission.dto.MissionQuizRequest;
+import com.kb.jjan.domain.mission.mission.repository.MissionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MissionService  {
+
+    private final MissionRepository missionRepository;
+
+    public void registerQuiz(MissionQuizRequest missionQuizRequest) {
+        Mission mission = missionQuizRequest.toEntity();
+        missionRepository.save(mission);
+    }
+}
