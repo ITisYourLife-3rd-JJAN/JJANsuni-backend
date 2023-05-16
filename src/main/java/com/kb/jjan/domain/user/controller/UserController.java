8c596194268c50f744c22d40b07e954bcb964e0c
gilyeon00 <12venus08@naver.com>
2023-05-16 17:45:30 +0900

    [#5]Feat: Create updateUser api - balance

--- src/main/java/com/kb/jjan/domain/user/controller/UserController.java
+++ src/main/java/com/kb/jjan/domain/user/controller/UserController.java
@@ -2,15 +2,13 @@ package com.kb.jjan.domain.user.controller;
 
 import com.kb.jjan.domain.user.User;
 import com.kb.jjan.domain.user.dto.UserRequest;
+import com.kb.jjan.domain.user.dto.UserUpdatePriceRequest;
 import com.kb.jjan.domain.user.service.UserService;
 import com.kb.jjan.global.result.ResultResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.kb.jjan.global.result.ResultCode.USER_REGISTRATION_SUCCESS;
 
@@ -27,6 +25,14 @@ public class UserController {
             throws Exception {
         userService.registerUser(userRequest);
         ResultResponse<User> resultResponse = new ResultResponse<>(USER_REGISTRATION_SUCCESS);
+        return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
+    }
+
+    @PatchMapping("/debit")
+    public ResponseEntity<ResultResponse> updateUser(@RequestBody UserUpdatePriceRequest userUpdatePriceRequest)
+            throws Exception {
+        int balance = userService.updateUser(userUpdatePriceRequest);
+        ResultResponse<Integer> resultResponse = new ResultResponse<>(USER_REGISTRATION_SUCCESS, balance);
         return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
     }
 
