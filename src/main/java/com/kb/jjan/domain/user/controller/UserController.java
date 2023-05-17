d6b237cc87b15d6fd880a875be5b85792c7dfa5b
seungukNoh <dnrdl8255@gmail.com>
2023-05-17 15:57:47 +0900

    로그인, 회원정보검색 기능 구현

--- src/main/java/com/kb/jjan/domain/user/controller/UserController.java
+++ src/main/java/com/kb/jjan/domain/user/controller/UserController.java
@@ -2,22 +2,18 @@ package com.kb.jjan.domain.user.controller;
 
 import com.kb.jjan.domain.user.User;
 import com.kb.jjan.domain.user.dto.UserRequest;
-import com.kb.jjan.domain.user.dto.UserUpdatePriceRequest;
-import com.kb.jjan.domain.user.service.FamilyCodeService;
 import com.kb.jjan.domain.user.service.UserService;
 import com.kb.jjan.global.result.ResultResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-import static com.kb.jjan.global.result.ResultCode.USER_GENERATION_SUCCESS;
 import static com.kb.jjan.global.result.ResultCode.USER_REGISTRATION_SUCCESS;
-import static com.kb.jjan.global.result.ResultCode.USER_UPDATE_BALANCE_SUCCESS;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RequestMapping("api/v1/users")
 @RestController
@@ -25,32 +21,12 @@ import java.util.Map;
 public class UserController {
 
     private final UserService userService;
-    private final FamilyCodeService familyCodeService;
 
     @PostMapping("/join")
     public ResponseEntity<ResultResponse> registerUser(@RequestBody UserRequest userRequest)
             throws Exception {
         userService.registerUser(userRequest);
         ResultResponse<User> resultResponse = new ResultResponse<>(USER_REGISTRATION_SUCCESS);
-        return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
-    }
-
-    @PatchMapping("/debit")
-    public ResponseEntity<ResultResponse> updateUser(@RequestBody UserUpdatePriceRequest userUpdatePriceRequest)
-            throws Exception {
-        int balance = userService.updateUser(userUpdatePriceRequest);
-
-        Map<String, Integer> item = new HashMap<>();
-        item.put("balance", balance);
-
-        ResultResponse<Integer> resultResponse = new ResultResponse<>( USER_UPDATE_BALANCE_SUCCESS, item);
-        return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
-    }
-
-    @GetMapping("/family-code")
-    public ResponseEntity<ResultResponse> generateFamilyCode() {
-        String famCode = familyCodeService.generateFamilyCode();
-        ResultResponse<String> resultResponse = new ResultResponse<>(USER_GENERATION_SUCCESS, famCode);
         return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
     }
 
