b37a2c6a5cf679a731d2b93a60daeee7949cb3a0
gilyeon00 <12venus08@naver.com>
2023-05-18 17:11:04 +0900

    [#23]Feat: 푼 문제수 증가시키는 logic 추가

--- src/main/java/com/kb/jjan/domain/mission/userMission/service/UserMissionService.java
+++ src/main/java/com/kb/jjan/domain/mission/userMission/service/UserMissionService.java
@@ -6,8 +6,8 @@ import com.kb.jjan.domain.mission.userMission.UserMission;
 import com.kb.jjan.domain.mission.userMission.dto.UserMissionRequest;
 import com.kb.jjan.domain.mission.userMission.repository.UserMissionRepository;
 import com.kb.jjan.domain.user.User;
-import com.kb.jjan.domain.user.dto.UserRequest;
 import com.kb.jjan.domain.user.repository.UserRepository;
+import com.kb.jjan.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,13 +19,15 @@ public class UserMissionService {
     private final UserMissionRepository userMissionRepository;
     private final MissionRepository missionRepository;
     private final UserRepository userRepository;
+    private final UserService userService;
 
 
     public void registerUserMission(@RequestBody UserMissionRequest userMissionRequest) throws Exception {
         Mission solvedMission = missionRepository.getReferenceById(userMissionRequest.getSolvedMissionId());
         User solvedUser = userRepository.getReferenceById(userMissionRequest.getSolvedUserId());
+        userService.updateUser(userMissionRequest.getSolvedUserId());
 
-        UserMission userMission = userMissionRequest.toEntity(solvedMission, solvedUser );
+        UserMission userMission = userMissionRequest.toEntity(solvedMission, solvedUser);
         userMissionRepository.save(userMission);
     }
 
