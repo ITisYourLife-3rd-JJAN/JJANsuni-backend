5eb3da57c3122e8426bd784f0a741653d4e27029
gilyeon00 <12venus08@naver.com>
2023-05-18 21:50:21 +0900

    [#23]Feat: Add findBySolvedUserId API

--- src/main/java/com/kb/jjan/domain/mission/userMission/controller/UserMissionController.java
+++ src/main/java/com/kb/jjan/domain/mission/userMission/controller/UserMissionController.java
@@ -8,13 +8,11 @@ import com.kb.jjan.global.result.ResultResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import static com.kb.jjan.global.result.ResultCode.USER_MISSION_REGISTRATION_SUCCESS;
-import static com.kb.jjan.global.result.ResultCode.USER_REGISTRATION_SUCCESS;
+import java.util.List;
+
+import static com.kb.jjan.global.result.ResultCode.*;
 
 @RequestMapping("api/v1/missions")
 @RestController
@@ -28,6 +26,14 @@ public class UserMissionController {
             throws Exception {
         userMissionService.registerUserMission(userMissionRequest);
         ResultResponse<UserMission> resultResponse = new ResultResponse<>(USER_MISSION_REGISTRATION_SUCCESS);
+        return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
+    }
+
+    @GetMapping("/mission/{userId}")
+    public ResponseEntity<ResultResponse> getMissionStatus(@PathVariable("userId") long userId)
+            throws Exception {
+        List<UserMission> userMissionList = userMissionService.getMissionStatus(userId);
+        ResultResponse<List<UserMission>> resultResponse = new ResultResponse<>(GET_USER_MISSION_SUCCESS, userMissionList);
         return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
     }
 }
