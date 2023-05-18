34038565803defd61992eac3445db8fd02053355
seungukNoh <dnrdl8255@gmail.com>
2023-05-18 21:05:01 +0900

    [#20]Feat: Response 수정 중

--- src/main/java/com/kb/jjan/domain/user/controller/UserController.java
+++ src/main/java/com/kb/jjan/domain/user/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.kb.jjan.domain.user.controller;
 
+import com.kb.jjan.domain.bank.debit.Debit;
 import com.kb.jjan.domain.user.User;
+import com.kb.jjan.domain.user.dto.UserFamilyResponse;
 import com.kb.jjan.domain.user.dto.UserLoginRequest;
 import com.kb.jjan.domain.user.dto.UserRequest;
 import com.kb.jjan.domain.user.dto.UserUpdatePriceRequest;
 import com.kb.jjan.domain.user.service.FamilyCodeService;
 import com.kb.jjan.domain.user.service.UserService;
+import com.kb.jjan.global.error.ErrorResponse;
 import com.kb.jjan.global.result.ResultResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 
+import static com.kb.jjan.global.error.ErrorCode.USER_FINDBYFAMCODE_ERROR;
 import static com.kb.jjan.global.result.ResultCode.USER_GENERATION_SUCCESS;
 import static com.kb.jjan.global.result.ResultCode.USER_REGISTRATION_SUCCESS;
 import static com.kb.jjan.global.result.ResultCode.USER_UPDATE_BALANCE_SUCCESS;
@@ -91,11 +95,14 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
     }
 
-    @PostMapping("find-family")
-    public ResponseEntity<ResultResponse> findByFamcode(@RequestBody long userId)
+    @GetMapping("find-family/{userId}")
+    public ResponseEntity<ResultResponse> findByFamCode(@PathVariable("userId") long userId)
             throws Exception{
-        User user = userService.findByFamcode(userId);
-        ResultResponse<User> resultResponse = new ResultResponse<>(USER_FINDBYFAMCODE_SUCCESS, user);
+        List<User> familyList = userService.findByFamCode(userId);
+        UserFamilyResponse userFamilyResponse = new UserFamilyResponse<>();
+
+
+        ResultResponse<> resultResponse = new ResultResponse<>(USER_FINDBYFAMCODE_SUCCESS, familyList);
         return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
     }
 
