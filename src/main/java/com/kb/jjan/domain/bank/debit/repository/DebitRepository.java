9fddf84dc8f7e98e35348ed26e61cdadd7f70b30
Subny <richsubin411@gmail.com>
2023-05-18 13:52:34 +0900

    (#15)FEAT : DebitHistory : senduserid, receiveduserid 안보이지만 일단 1차 구현

--- src/main/java/com/kb/jjan/domain/bank/debit/repository/DebitRepository.java
+++ src/main/java/com/kb/jjan/domain/bank/debit/repository/DebitRepository.java
@@ -5,10 +5,12 @@ import com.kb.jjan.domain.bank.debit.dto.DebitRequest;
 import com.kb.jjan.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface DebitRepository extends JpaRepository<Debit, Long> {
-    @Query("SELECT d FROM Debit d WHERE d.sendUser = :userId OR d.receivedUser = :userId")
-    List<Debit> findByUserId(Long userId);
+//    @Query("SELECT d FROM Debit d WHERE d.sendUser.userId = :userId OR d.receivedUser.userId = :userId")
+//    List<Debit> findBySendUserIdOrReceivedUserId(@Param("userId") long sendUserId, @Param("userId") long receivedUserId);
+    List<Debit> findBySendUserUserIdOrReceivedUserUserId(long userId1, long userId2);
 }
